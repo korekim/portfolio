1fefa9172e2489e496d3c784b745173aa401b9a0
Jacob Kim <jacobjwkim@gmail.com>
2025-08-26 19:27:16 -0600

    fixed apostrophes

--- app/blog/defcon33/page.tsx
+++ app/blog/defcon33/page.tsx
@@ -9,7 +9,7 @@ const Defcon33Page: React.FC = () => (
                     <i>This post is copied from my LinkedIn</i>
                     <br />
                     <br />
-                    Here's a late DefCon post! (Same month counts, right?)
+                    Here&apos;s a late DefCon post! (Same month counts, right?)
                     <br />
                     <br />
                     tl;dr: I met amazing people, learned from them, and won my first CTF!
@@ -18,7 +18,7 @@ const Defcon33Page: React.FC = () => (
                     This year I attended my first DefCon. I had no idea what to expect, as I had only really heard about it from the Darknet Diaries podcast.
                     <br />
                     <br />
-                    Because this was my first ever cybersecurity event, I organized a climbing meetup on Day 1. Since I'm very comfortable socializing in a climbing gym, I figured that I could make use of my sport in order to create some connections in the cyber world. I quickly met the witty Luke McGuinness, and we quickly bonded over a shared interest in both scaling small rocks and breaking into computer systems. (A small caveat is that Luke is a full-fledged pentester and I have merely dreamt of such activities).
+                    Because this was my first ever cybersecurity event, I organized a climbing meetup on Day 1. Since I&apos;m very comfortable socializing in a climbing gym, I figured that I could make use of my sport in order to create some connections in the cyber world. I quickly met the witty Luke McGuinness, and we quickly bonded over a shared interest in both scaling small rocks and breaking into computer systems. (A small caveat is that Luke is a full-fledged pentester and I have merely dreamt of such activities).
                     <br />
                     <br />
                     In the afternoon, I spent some time in the merch line chatting with David Broggy. A funny happenstance is that he and my dad, and his son and me both share the same name (Dave and Jake, respectively). I was very grateful to have the opportunity to talk with someone who has so much experience in the field.
@@ -31,10 +31,10 @@ const Defcon33Page: React.FC = () => (
                     . That night, I attended the Bishop Fox party with Luke and met David Rushmer, MSc, Christopher Hansen, Peter Van Splunder, and James, whom I forgot to exchange contact information with. Everyone had such interesting jobs, and I had a blast listening to stories about engagments and showing off my shiny new Keysight hardware hacking case that I had earned in the CTF.
                     <br />
                     <br />
-                    I'm running out of characters, so I'll make the rest short. The next couple of days I attended talks about OT and ICS security by Mike Holcomb, and hung out with the group whilst meandering around the various villages.
+                    I'm running out of characters, so I&apos;ll make the rest short. The next couple of days I attended talks about OT and ICS security by Mike Holcomb, and hung out with the group whilst meandering around the various villages.
                     <br />
                     <br />
-                    All in all, it was an amazing event, and I can't wait to attend next year! Thanks to everyone who made it a great time, and I hope to see y'all soon!
+                    All in all, it was an amazing event, and I can&apos;t wait to attend next year! Thanks to everyone who made it a great time, and I hope to see y&apos;all soon!
                     <br />
                     <br />
                 </p>
